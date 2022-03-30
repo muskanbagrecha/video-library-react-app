e7bbbf1cbc99811b76fab6191823b94582e2f8cd
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-03-31 01:09:49 +0530

    added video listing page, updated db

--- src/Assets/Images/index.js
+++ src/Assets/Images/index.js
@@ -2,8 +2,47 @@ import outfit from "./Categories/outfit.webp";
 import skincare from "./Categories/skincare.webp";
 import thumbnail1 from "./Thumbnail/thumbnail1.webp";
 import thumbnail2 from "./Thumbnail/thumbnail2.webp";
+import thumbnail3 from "./Thumbnail/thumbnail3.webp";
+import thumbnail4 from "./Thumbnail/thumbnail4.webp";
+import thumbnail5 from "./Thumbnail/thumbnail5.webp";
+import thumbnail6 from "./Thumbnail/thumbnail6.webp";
+import thumbnail7 from "./Thumbnail/thumbnail7.webp";
+import thumbnail8 from "./Thumbnail/thumbnail8.webp";
+import thumbnail9 from "./Thumbnail/thumbnail9.webp";
 import glimpse1 from "./VideoGlimpse/glimpse1.webp";
+import glimpse2 from "./VideoGlimpse/glimpse2.webp";
+import glimpse3 from "./VideoGlimpse/glimpse3.webp";
+import glimpse4 from "./VideoGlimpse/glimpse4.webp";
+import glimpse5 from "./VideoGlimpse/glimpse5.webp";
+import glimpse6 from "./VideoGlimpse/glimpse6.webp";
+import glimpse7 from "./VideoGlimpse/glimpse7.webp";
+import glimpse8 from "./VideoGlimpse/glimpse8.webp";
+import glimpse9 from "./VideoGlimpse/glimpse9.webp";
+
 import creator1 from "./Creators/creator1.webp";
 import creator2 from "./Creators/creator2.webp";
 
-export { outfit, skincare, thumbnail1, thumbnail2, glimpse1, creator1 };
+export {
+  outfit,
+  skincare,
+  thumbnail1,
+  thumbnail2,
+  thumbnail3,
+  thumbnail4,
+  thumbnail5,
+  thumbnail6,
+  thumbnail7,
+  thumbnail8,
+  thumbnail9,
+  glimpse1,
+  glimpse2,
+  glimpse3,
+  glimpse4,
+  glimpse5,
+  glimpse6,
+  glimpse7,
+  glimpse8,
+  glimpse9,
+  creator1,
+  creator2,
+};
