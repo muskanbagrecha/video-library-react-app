400f272d719b4c784afea395c7ecd36298f32cba
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-04-05 19:12:50 +0530

    Updated playlist page

--- src/Pages/PlaylistPage/Components/PlaylistCard/PlaylistCard.jsx
+++ src/Pages/PlaylistPage/Components/PlaylistCard/PlaylistCard.jsx
@@ -1,15 +1,41 @@
 import { Card } from "../../../../Components/UI";
+import { useNavigate } from "react-router-dom";
+import { usePlaylist, useAlert, useAuth } from "../../../../CustomHooks";
 import "./PlaylistCard.css";
-export const PlaylistCard = ({ title, description, videos }) => {
+
+export const PlaylistCard = ({ _id, title }) => {
+  const navigate = useNavigate();
+  const { deletePlaylist } = usePlaylist();
+  const { setShowAlert } = useAlert();
+  const {
+    authState: { token },
+  } = useAuth();
+  const selectPlaylistHandler = () => {
+    navigate("/playlist/" + _id);
+  };
+
+  const deletePlaylistHandler = () => {
+    deletePlaylist({ playlistId: _id, token });
+    setShowAlert({
+      showAlert: true,
+      alertMessage: "Playlist has been deleted",
+      type: "info",
+    });
+    navigate("/playlist");
+  };
+
   return (
-    <Card className="playlist-card card-vertical">
-      {/* <div className="playlist-thumbnail">
-        <img src={playlist.thumbnail} alt={playlist.title} />
-      </div> */}
-      <div className="playlist-info">
-        <h3 className="playlist-title">{title}</h3>
-        <p className="playlist-description">{description}</p>
-      </div>
-    </Card>
+    <div onClick={selectPlaylistHandler}>
+      <Card className="playlist-card card-vertical">
+        <div className="card-top-right" onClick={deletePlaylistHandler}>
+          <i className="fa-solid fa-trash"></i>
+        </div>
+        <div className="playlist-info">
+          <h3 className="playlist-title" title={title}>
+            {title}
+          </h3>
+        </div>
+      </Card>
+    </div>
   );
 };
