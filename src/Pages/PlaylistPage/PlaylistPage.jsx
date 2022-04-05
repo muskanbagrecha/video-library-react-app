6ee7af16603e96148589b3241a5f51a4bdb2489d
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-04-05 19:43:54 +0530

    added create playlist option on playlist page

--- src/Pages/PlaylistPage/PlaylistPage.jsx
+++ src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -1,8 +1,63 @@
-import { usePlaylist } from "../../CustomHooks";
+import { useState } from "react";
+import { usePlaylist, useAuth, useAlert } from "../../CustomHooks";
 import { PlaylistCard } from "./Components/PlaylistCard/PlaylistCard";
 import "./PlaylistPage.css";
 export const PlaylistPage = () => {
-  const { playlists } = usePlaylist();
+  const initialInput = {
+    title: "",
+    description: "",
+    error: null,
+  };
+
+  const { playlists, createPlaylist } = usePlaylist();
+  const [showInputField, setShowInputField] = useState(false);
+  const [playlistInput, setPlaylistInput] = useState(initialInput);
+  const {
+    authState: { token, isAuthenticated },
+  } = useAuth();
+  const { setShowAlert } = useAlert();
+
+  const inputChangeHandler = (event) => {
+    setPlaylistInput({
+      ...playlistInput,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const createPlaylistHandler = (e) => {
+    e.preventDefault();
+    if (!isAuthenticated) {
+      setShowAlert({
+        showAlert: true,
+        alertMessage: "You must be logged in to create a playlist",
+        type: "danger",
+      });
+      return;
+    }
+    if (!showInputField) {
+      setShowInputField(true);
+      return;
+    }
+    if (playlistInput.title.trim() === "") {
+      setPlaylistInput({ ...playlistInput, error: "Title is required" });
+      return;
+    }
+    setPlaylistInput({ ...playlistInput, error: null });
+    createPlaylist({
+      playlist: {
+        title: playlistInput.title,
+        description: playlistInput.description,
+      },
+      token,
+    });
+    setShowAlert({
+      showAlert: true,
+      alertMessage: "Playlist Created Successfully",
+      type: "success",
+    });
+    setPlaylistInput({ title: "", description: "", error: null });
+    setShowInputField(false);
+  };
 
   return (
     <div className="sub-container">
@@ -22,7 +77,30 @@ export const PlaylistPage = () => {
             ))}
           </div>
         )}
-        <button className="btn btn-primary">Create New</button>
+        {showInputField && (
+          <form className="input-form playlist-form">
+            <input
+              type="text"
+              placeholder="title"
+              onChange={inputChangeHandler}
+              name="title"
+              value={playlistInput.title}
+            />
+            <input
+              type="text"
+              placeholder="description"
+              onChange={inputChangeHandler}
+              name="description"
+              value={playlistInput.description}
+            />
+            {playlistInput.error && (
+              <p className="red">{playlistInput.error}</p>
+            )}
+          </form>
+        )}
+        <button className="btn btn-primary" onClick={createPlaylistHandler}>
+          Create New
+        </button>
       </div>
     </div>
   );
